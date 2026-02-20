7f24e0669cca81f7a66cb58087169440761af564
Shasaninisansal <91341553+MasterBlacky007@users.noreply.github.com>
2026-02-20 14:42:02 +0530

    create backend  ms

---
+++ backend/Planner-ms/src/main/java/com/Planner_ms/PlannerMsApplication.java
@@ -0,0 +1,13 @@
+package com.Planner_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerMsApplication.class, args);
+	}
+
+}
